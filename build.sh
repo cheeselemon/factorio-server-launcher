1c52530b74c4cefeeebcb342599314baad37e9d3
cheeselemon <cheeselemon.yarn@gmail.com>
2024-01-22 12:08:20 +0900

    feat: update scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 docker build -t factorio . 
+
+# build without cache for updates 
+sudo docker build -t factorio . --no-cache
--- run.sh
+++ run.sh
@@ -5,3 +5,14 @@ docker run -d \
   -v /home/ubuntu/factorio-data:/factorio/parent \
   -e FACTORIO_HOME=/home/ubuntu/factorio \
   factorio
+
+# remove container 
+sudo docker remove factorio
+
+sudo docker run -d \
+  --name factorio \
+  -p 34197:34197/udp \
+  -p 27015:27015/tcp \
+  -v /home/ubuntu/factorio-data:/factorio/parent \
+  -e FACTORIO_HOME=/home/ubuntu/factorio \
+  factorio
